36d6d6b034e51b686e863628aafad41df7354458
A03ki <a03ki04@gmail.com>
2020-12-01 21:31:25 +0900

    データオブジェクトの追加

---
+++ ueca/data.py
@@ -0,0 +1,65 @@
+import pint
+
+
+from typing import Any
+
+
+ureg = pint.UnitRegistry()
+
+
+class PhysicsData:
+    def __init__(self, magnitude, unit: str, left_side: str = "") -> None:
+        self.data = ureg.Quantity(magnitude, unit)
+        self.left_side = left_side
+
+    @property
+    def magnitude(self) -> Any:
+        return self.data.magnitude
+
+    @property
+    def unit(self) -> str:
+        return str(self.data.units)
+
+    def __add__(self, other: "PhysicsData") -> "PhysicsData":
+        new_data = self.data + other.data
+        return PhysicsData(new_data.magnitude, str(new_data.units))
+
+    def __sub__(self, other: "PhysicsData") -> "PhysicsData":
+        new_data = self.data - other.data
+        return PhysicsData(new_data.magnitude, str(new_data.units))
+
+    def __mul__(self, other: Any) -> "PhysicsData":
+        if isinstance(other, PhysicsData):
+            data = other.data
+        new_data = self.data * data
+        return PhysicsData(new_data.magnitude, str(new_data.units))
+
+    def __floordiv__(self, other: Any) -> "PhysicsData":
+        if isinstance(other, PhysicsData):
+            data = other.data
+        new_data = self.data // data
+        return PhysicsData(new_data.magnitude, str(new_data.units))
+
+    def __truediv__(self, other: Any) -> "PhysicsData":
+        if isinstance(other, PhysicsData):
+            data = other.data
+        new_data = self.data / data
+        return PhysicsData(new_data.magnitude, str(new_data.units))
+
+    def __repr__(self) -> str:
+        return str(self.data)
+
+    def _repr_html_(self) -> str:
+        return self.__repr__()
+
+    def _repr_latex_(self, symbolic: bool = True) -> str:
+        latex_spec = "{:~L}"
+        if not symbolic:
+            latex_spec = latex_spec.replace("~", "")
+
+        text = latex_spec.format(self.data)
+
+        if self.left_side != "":
+            text = f"{self.left_side} = {text}"
+
+        return text
